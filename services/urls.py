b3a24f2efca78d8f84f74bf13ec0a9bd8936a435
Kes Cardoso <kesia.development@gmail.com>
2020-08-31 13:19:24 +0000

    Added services views and templates

--- services/urls.py
+++ services/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.all_services, name='services')
 ]
--- services/views.py
+++ services/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
 
-# Create your views here.
+from .models import Service, Category
+
+
+def all_services(request):
+    """ A view to show all services, including sorting and search queries """
+
+    services = Service.objects.all()
+
+    context = {
+        'services': services,
+    }
+
+    return render(request, 'services/services.html', context)
