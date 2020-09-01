99a2594d48fecf67ba0419ecf3c46fb087d3599f
Kes Cardoso <kesia.development@gmail.com>
2020-09-01 13:47:37 +0000

    Added search functionality

--- services/views.py
+++ services/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Service
 
 
@@ -6,9 +8,23 @@ def all_services(request):
     """ A view to show all services, including sorting and search queries """
 
     services = Service.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,
+                               "You didn't enter any search criteria.")
+                return redirect(reverse('services'))
+
+            """ Search queries in name or description """
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            services = services.filter(queries)
 
     context = {
         'services': services,
+        'search_term': query,
     }
 
     return render(request, 'services/services.html', context)
