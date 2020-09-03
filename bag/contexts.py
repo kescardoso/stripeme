6cd9e9dec3a2df4c83b3654e4ed2855b91c0694d
Kes Cardoso <kesia.development@gmail.com>
2020-09-03 13:21:45 +0000

    Updated bag context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from services.models import Service
 
 
 def bag_contents(request):
@@ -7,12 +9,25 @@ def bag_contents(request):
     bag_items = []
     total = 0
     service_count = 0
+    bag = request.session.get('bag', {})
 
-    # 10% Off Promo
+    for item_id, item_data in bag.items():
+        """ View a list of items in bag """
+        service = get_object_or_404(Service, pk=item_id)
+        total += item_data * service.price
+        service_count += item_data
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': item_data,
+            'service': service,
+        })
+
     if total < settings.TEN_OFF_THRESHOLD:
+        """ 10% Off Promo using $100 as purchase threshold """
         # If order counts to the minimum amount in TEN_OFF_THRESHOLD
         # then user can get 10% off with STANDARD_PROMO_PERCENTAGE.
         # Decimal is prefered and more accurate for financial transactions.
+
         promo = total * Decimal(settings.STANDARD_PROMO_PERCENTAGE / 100)
         ten_off_delta = settings.TEN_OFF_THRESHOLD - total
     else:
--- bag/views.py
+++ bag/views.py
@@ -27,5 +27,4 @@ def add_to_bag(request, item_id):
         messages.success(request, f'Added {service.name} to your bag')
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
