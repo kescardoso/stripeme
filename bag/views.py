70394c5838888e4ad9de3c55905bef6946c36f8b
Kes Cardoso <kesia.development@gmail.com>
2020-09-03 13:09:15 +0000

    Added ad to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -6,4 +6,26 @@ from services.models import Service
 
 def view_bag(request):
     """ A view that renders the bag contents page """
+
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    service = get_object_or_404(Service, pk=item_id)
+    quantity = int(request.POST.get('quantity'))  # Service quantity in bag
+    redirect_url = request.POST.get('redirect_url')  # Url redirect from bag
+    bag = request.session.get('bag', {})  # Bag content is stored in session
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+        messages.success(
+            request, f'Updated {service.name} quantity to {bag[item_id]}')
+    else:
+        bag[item_id] = quantity
+        messages.success(request, f'Added {service.name} to your bag')
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
