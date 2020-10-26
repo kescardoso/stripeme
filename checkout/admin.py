8ff2d8cc54e047b8ff44a9a7da727c95c6bf37ea
Kes Cardoso <kesia.development@gmail.com>
2020-10-26 18:42:10 +0000

    Added order history and updated checkout views

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,7 +12,7 @@ class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ('order_number', 'date',
                        'promo_cost', 'order_total',
                        'grand_total', 'original_bag', 'stripe_pid')
-    fields = ('order_number', 'date', 'full_name', 'email',
+    fields = ('order_number', 'user_profile', 'date', 'full_name', 'email',
               'phone_number', 'postcode', 'town_or_city',
               'street_address1', 'street_address2', 'county',
               'promo_cost', 'order_total', 'grand_total',
--- profiles/views.py
+++ profiles/views.py
@@ -31,15 +31,15 @@ def profile(request):
     return render(request, template, context)
 
 
-# def order_history(request, order_number):
-#     order = get_object_or_404(Order, order_number=order_number)
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
 
-#     messages.info(
-#         request,
-#         (f'This is a past confirmation for order number {order_number}.'
-#          'A confirmation email was sent on the order date.'))
+    messages.info(
+        request,
+        (f'This is a past confirmation for order number {order_number}.'
+         'A confirmation email was sent on the order date.'))
 
-#     template = 'checkout/checkout_success.html'
-#     context = {'order': order, 'form-profile': True}
+    template = 'checkout/checkout_success.html'
+    context = {'order': order, 'form-profile': True}
 
     return render(request, template, context)
