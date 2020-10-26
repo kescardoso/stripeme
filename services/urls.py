d8bb2ae8171882fb3c6f7871f921b387ae111654
Kes Cardoso <kesia.development@gmail.com>
2020-10-26 20:44:11 +0000

    Added add product functionality

--- services/urls.py
+++ services/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_services, name='services'),
-    path('<service_id>', views.service_detail, name='service_detail'),
+    path('<int:service_id>/', views.service_detail, name='service_detail'),
+    path('add/', views.add_service, name='add_service'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 from django.db.models.functions import Lower
 from .models import Service, Category
+from .forms import ServiceForm
 
 
 def all_services(request):
@@ -70,3 +72,75 @@ def service_detail(request, service_id):
     }
 
     return render(request, 'services/service_detail.html', context)
+
+
+@login_required
+def add_service(request):
+    """ Add a service to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, request.FILES)
+        if form.is_valid():
+            service = form.save()
+            messages.success(request, 'Successfully added service!')
+            return redirect(reverse('service_detail', args=[service.id]))
+        else:
+            messages.error(
+                request,
+                'Failed to add service. Please ensure the form is valid.')
+    else:
+        form = ServiceForm()
+
+    template = 'services/add_service.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_service(request, service_id):
+    """ Edit a service in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    service = get_object_or_404(Service, pk=service_id)
+    if request.method == "POST":
+        form = ServiceForm(request.POST, request.FILES, instance=service)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated service!')
+            return redirect(reverse('service_detail', args=[service.id]))
+        else:
+            messages.error(
+                request,
+                'Failled to update service. Please ensure the form is valid.')
+    else:
+        form = ServiceForm(instance=service)
+        messages.info(request, f'You are editing {service.name}')
+
+    template = 'services/edit_service.html'
+    context = {
+        'form': form,
+        'service': service,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_service(request, service_id):
+    """ Delete a service from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    service = get_object_or_404(Service, pk=service_id)
+    service.delete()
+    messages.success(request, 'Service deleted!')
+    return redirect(reverse('sevrices'))
